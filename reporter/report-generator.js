02257b4790964d3024e460c169e7dcf12e2dc618
hardymj <30835187+hardymj@users.noreply.github.com>
2024-08-12 14:36:45 +0100

    Update report-generator.js

--- reporter/report-generator.js
+++ reporter/report-generator.js
@@ -2,4 +2,4 @@ const {CucumberConverter} = require('cucumber-to-junit');
 const converter = new CucumberConverter({
     markUndefinedAsFailed: true,
 });
-converter.convertToJunit('reports/report.json', 'reports/report.xml');
+converter.convertToJunit('reports/cucumber.json', 'reports/report.xml');
