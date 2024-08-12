3ed37c80ce23be5d0d6176826a8c2069d97031a4
hardymj <30835187+hardymj@users.noreply.github.com>
2024-08-12 13:56:32 +0100

    Update report-generator.js

--- reporter/report-generator.js
+++ reporter/report-generator.js
@@ -1,12 +1,5 @@
-const reporter = require('cucumber-html-reporter');
-const options = {
-    theme: 'hierarchy',
-    jsonFile: 'reports/cucumber.json',
-    output: 'reports/cucumber_report.html',
-    reportSuiteAsScenarios: true,
-    scenarioTimestamp: true,
-    launchReport: false,
-    failedSummaryReport: false,
-};
-
-reporter.generate(options);
+const {CucumberConverter} = require('cucumber-to-junit');
+const converter = new CucumberConverter({
+    markUndefinedAsFailed: true,
+});
+converter.convertToJunit('reports/report.jsonn', 'reports/report.xml');
