ef98e68973aededbbd82392b427518b08f761821
hardymj <30835187+hardymj@users.noreply.github.com>
2024-08-15 09:51:04 +0100

    Update testData.js

--- data/testData.js
+++ data/testData.js
@@ -1,14 +1,8 @@
 'use strict';
-
-const moment = require('moment');
-
 class TestData {
     constructor() {
-        this._currentDate = moment();
     }
     init() {
     }
 }
-
 module.exports = TestData;
-
